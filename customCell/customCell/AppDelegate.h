b6e95a4fc9670f3d3b7f681efec2504f9061ae1c
Strelka <yu.a.novoselova@gmail.com>
2017-04-14 10:43:16 +0300

    customCell

---
+++ customCell/customCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  customCell
+//
+//  Created by Jullia Sharaeva on 14.04.17.
+//  Copyright © 2017 Julia Sharaeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ customCell/customCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  customCell
+//
+//  Created by Jullia Sharaeva on 14.04.17.
+//  Copyright © 2017 Julia Sharaeva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
